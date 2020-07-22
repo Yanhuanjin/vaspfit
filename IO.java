1c03584ef78a58a294eadf6e8da6095c95f616af
jinyanhuan <jinyanhuan@gmail.com>
2020-07-22 13:26:37 +0800

    modified NELECT

--- IO.java
+++ IO.java
@@ -1,6 +1,3 @@
-import com.sun.javafx.scene.control.skin.VirtualFlow;
-import com.sun.jmx.remote.internal.ArrayQueue;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -8,6 +5,9 @@ import java.io.IOException;
 import java.util.*;
 
 public class IO {
+    public static String NELECT;
+    public static String FERMI;
+
     public static void main(String[] args) {
         File f = new File("C:/Users/yanfa/Desktop/OUTCAR");
         try (FileReader fr = new FileReader(f);
@@ -17,15 +17,20 @@ public class IO {
                 if (null == line)
                     break;
                 if (line.contains("NELECT")){
-                    List<String> e = Arrays.asList(line.trim().split("\\s+"));
-                    System.out.println(e.get(2));
+                    NELECT = getNELECT(line);
                 }
+
             }
-                //if (line.contains("E-fermi"))
-                    //System.out.println(line);
+            System.out.println(NELECT);
+
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    public static String getNELECT(String line){
+        List<String> nelect = Arrays.asList(line.trim().split("\\s+"));
+        return nelect.get(2);
     }
 
 
